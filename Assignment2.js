5b5a5a1dc5b9d9519f4bdac87984c31b804de630
Anand prabhakar <anandprabhakar0507@gmail.com>
2021-01-28 07:39:56 +0530

    Add files via upload

---
+++ Assignment2.js
@@ -0,0 +1,10 @@
+const clothes = ['jacket', 't-shirt'];
+clothes.length = 0;
+console.log(clothes[0]);
+
+var array = [55,27,230,56,70,1];
+sum = 0
+var i;
+for(i = 0; i<array.length; i++){
+sum = sum + array[i];}
+console.log("Sum of Numbers in array is: " +sum);
