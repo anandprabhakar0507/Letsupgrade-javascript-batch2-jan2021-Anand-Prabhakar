fdf8f280b8d5994339c54e987234e6e7eb68836a
Restyled.io <commits@restyled.io>
2021-10-09 16:47:40 +0000

    Restyled by clang-format

--- Assignment2.js
+++ Assignment2.js
@@ -1,11 +1,12 @@
-const clothes = ['jacket', 't-shirt', 'trouser', 'jeans'];
+const clothes = [ 'jacket', 't-shirt', 'trouser', 'jeans' ];
 console.log(clothes)
 clothes.length = 0;
 console.log(clothes[0]);
 
-var array = [55,27,230,56,70,1];
+var array = [ 55, 27, 230, 56, 70, 1 ];
 sum = 0
 var i;
-for(i = 0; i<array.length; i++){
-sum = sum + array[i];}
-console.log("Sum of Numbers in array is: " +sum);
+for (i = 0; i < array.length; i++) {
+  sum = sum + array[i];
+}
+console.log("Sum of Numbers in array is: " + sum);
