428512b40ab0b7b1b6cdc511805ce53f9f3259fc
Restyled.io <commits@restyled.io>
2021-10-09 16:52:32 +0000

    Restyled by prettier

--- Assignment3.js
+++ Assignment3.js
@@ -1,12 +1,11 @@
-
-
 var library = [
-  {author : 'Bill Gates', title : 'The Road Ahead', readingStatus : true},
-  {author : 'Steve Jobs', title : 'Walter Isaacson', readingStatus : true}, {
-    author : 'Suzanne Collins',
-    title : 'Mockingjay: The Final Book of The Hunger Games',
-    readingStatus : false
-  }
+  { author: "Bill Gates", title: "The Road Ahead", readingStatus: true },
+  { author: "Steve Jobs", title: "Walter Isaacson", readingStatus: true },
+  {
+    author: "Suzanne Collins",
+    title: "Mockingjay: The Final Book of The Hunger Games",
+    readingStatus: false,
+  },
 ];
 
 /*Already read 'Bill Gates' by The Road Ahead.
@@ -16,12 +15,18 @@ Suzanne Collins.*/
 
 var i = 0;
 for (i; i < library.length; i++) {
-  if (library[i]['readingStatus'] == true) {
-    console.log("Already read \'" + library[i]['author'] + "\' by " +
-                library[i]['title']);
+  if (library[i]["readingStatus"] == true) {
+    console.log(
+      "Already read '" + library[i]["author"] + "' by " + library[i]["title"]
+    );
   } else {
-    console.log("You still need to read \'" + library[i]['title'] + "\' by " +
-                library[i]['author'] + "Book");
+    console.log(
+      "You still need to read '" +
+        library[i]["title"] +
+        "' by " +
+        library[i]["author"] +
+        "Book"
+    );
   }
 }
 
