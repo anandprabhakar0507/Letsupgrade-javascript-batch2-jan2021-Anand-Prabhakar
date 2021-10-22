90ad0a7c838feabb35d82fa0d097b0097166c90e
Restyled.io <commits@restyled.io>
2021-10-22 12:29:08 +0000

    Restyled by prettier

--- Assignment2.js
+++ Assignment2.js
@@ -1,11 +1,11 @@
-const clothes = [ 'jacket', 't-shirt', 'trouser', 'jeans' ];
-console.log(clothes)
+const clothes = ["jacket", "t-shirt", "trouser", "jeans"];
+console.log(clothes);
 clothes.length = 0;
 console.log(clothes[0]);
 
-var array = [ 55, 27, 230, 56, 70, 1 ];
-sum = 0
-dif = 0
+var array = [55, 27, 230, 56, 70, 1];
+sum = 0;
+dif = 0;
 var i;
 for (i = 0; i < array.length; i++) {
   sum = sum + array[i];
